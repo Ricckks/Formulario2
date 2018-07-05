4f50f8677b9b16e5738807535e62ee911c9a4871
Ricardo Andrade <38958597+Ricckks@users.noreply.github.com>
2018-07-05 13:48:52 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+let validation = function() {
+        let formName = document.getElementById('formName');
+        let formEmail = document.getElementById('formEmail');
+        let formConfirmaEmail = document.getElementById('formConfirmaEmail');
+        let formSenha = document.getElementById('formSenha');
+        let formConfirmaSenha = document.getElementById('formConfirmaSenha');
+
+        let formulario = [formName, formEmail, formConfirmaEmail, formSenha, formConfirmaSenha];
